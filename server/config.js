ac93f1ddc1d7cc22e730e796500e6d297dffc404
Blank <blank@BlankPC.localdomain>
2025-04-06 13:06:09 +0200

    Removed server

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3001,
-    clientOrigin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
-    maxPlayersPerRoom: {
-        trivia: 3,
-        puzzle: 5,
-    },
-};
